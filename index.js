5ffd7636eb631c66120ed713d90f4e6a522cd865
bdurb <brian@rpmgreenville.com>
2018-12-03 16:48:12 -0800

    working on url request

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const request = require('request')
 const cheerio = require('cheerio')
 
-const url = 'https://adventofcode.com/2018/day/1/input'
+const aocurl = 'https://adventofcode.com/2018/day/1/input'
 
-request(url, (err, res, html) => {
-  if (!err & res.statusCode == 200) {
-    const $ = cheerio.load(html)
-  }
+const frequencies = []
+
+request(aocurl, (err, res, html) => {
+  console.log(html)
 })
